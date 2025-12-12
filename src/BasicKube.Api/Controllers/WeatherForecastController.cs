fc9bd018554b0a0ba4495d1dccc889ad281ee23a
dmytro babych <memory1024@gmail.com>
2025-12-12 16:12:02 +0200

    - WeatherForecastController

--- src/BasicKube.Api/Controllers/WeatherForecastController.cs
+++ src/BasicKube.Api/Controllers/WeatherForecastController.cs
@@ -8,13 +8,20 @@ namespace BasicKubeApi.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherController : ControllerBase
+    public class WeatherForecastController : ControllerBase
     {
-        private readonly IMediator _mediator;
+        private static readonly string[] Summaries =
+        [
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        ];
 
-        public WeatherController(IMediator mediator)
+        private readonly IMediator _mediator;
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IMediator mediator)
         {
             _mediator = mediator;
+            _logger = logger;
         }
 
         [HttpGet("temperature")]
@@ -22,11 +29,25 @@ namespace BasicKubeApi.Controllers
             [FromQuery] double lat,
             [FromQuery] double lon)
         {
+            _logger.LogInformation("temperature");
             return await _mediator.Send(new GetTempQuery
             {
                 Lat = lat,
                 Lon = lon
             });
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<object> Get()
+        {
+            _logger.LogInformation("GetWeatherForecast");
+            return Enumerable.Range(1, 5).Select(index => new
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
         }
     }
 }
